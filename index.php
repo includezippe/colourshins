9995304c168df28e26fcf8454ef9ba4423900e90
includezippe <rziple@gmail.com>
2014-02-06 22:23:36 +0400

    Обозначена структура MVC

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-    header('Content-type: text/html; Charset=utf-8');
-    error_reporting(E_ALL);
-    define("Q_PATH", dirname(__FILE__));
 
+    header('Content-type: text/html; Charset=utf-8');
+
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    ini_set('display_errors', '1');
+
+    define("Q_PATH", dirname(__FILE__));
 
     include Q_PATH."/boot.php";
 
