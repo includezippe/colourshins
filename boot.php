7cff102d8f61e46c117819605f55f6197ce76546
includezippe <rziple@gmail.com>
2014-02-06 22:16:37 +0400

    Обозначена структура MVC

--- boot.php
+++ boot.php
@@ -1,8 +1,9 @@
 <?php
 
-include Q_PATH.'/logic/controller.class.php';
 include Q_PATH.'/logic/model.class.php';
 include Q_PATH.'/logic/view.class.php';
+include Q_PATH.'/logic/controller.class.php';
+
 include Q_PATH.'/logic/route.class.php';
 
 Route::Start();
--- view/_index.php
+++ view/_index.php
@@ -1,3 +1,4 @@
+index
 <?php
 /**
  * Created by PhpStorm.
