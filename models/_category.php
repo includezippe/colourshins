2fd01c0aec1fa4bd2d29cca1ba52e062dc60bfd0
Ilya Pavlukov <webmaster@seversiter.ru>
2014-02-10 00:23:01 +0400

    Доавблены пояснительные кометарии к методам

--- models/_category.php
+++ models/_category.php
@@ -1,10 +1,16 @@
 <?php
 
 class Model_category extends Model {
+	private $subjects_on_page = 5;
 
 	function __construct() {
 
 	}
+
+	/**
+	 * Возвращает истину если категория сущетсвует, и на оборот
+	 * @return boolean
+	 */
 
 	public function issetStore($cat_id){
 		$db = new PDO('mysql:host=colourshin.mysql;dbname=colourshin_db', 'colourshin_main', 'hrgmdjnd'); $db->exec('SET NAMES utf8');
@@ -21,6 +27,11 @@ class Model_category extends Model {
 		}
 	}
 
+	/**
+	 * Возвращает имя катеории по id
+	 * @return string
+	 */
+
 	public function getCategoryName($cat_id){
 		$db = new PDO('mysql:host=colourshin.mysql;dbname=colourshin_db', 'colourshin_main', 'hrgmdjnd'); $db->exec('SET NAMES utf8');
 
@@ -31,6 +42,10 @@ class Model_category extends Model {
 		return $values;
 	}
 
+	/**
+	 * Возвращает массив товаров
+	 * @return array
+	 */
 
 	public function getList($cat_id, $page){
 		$db = new PDO('mysql:host=colourshin.mysql;dbname=colourshin_db', 'colourshin_main', 'hrgmdjnd'); $db->exec('SET NAMES utf8');
@@ -39,9 +54,35 @@ class Model_category extends Model {
 
 		$count = intval($pRes[0][0]);
 
-		$values = $db->query('SELECT * FROM `store` WHERE `cat_id` = "'.$cat_id.'"'); $values = $values->fetchAll();
+		$num_pages = ceil($count / $this->subjects_on_page);
+
+		//Если страница больше чем максимальная, то она равна максимальной
+		if($page > $num_pages) $page = $num_pages;
+
+		//С чего начинать отсчет
+		$start = ($page - 1) * $this->subjects_on_page;
+
+		$values = $db->query('SELECT * FROM `store` WHERE `cat_id` = "'.$cat_id.'"  ORDER BY `id` DESC LIMIT '.$start.','.$this->subjects_on_page); 
+		$values = $values->fetchAll();
 
 		return $values;
+	}
+
+	/**
+	 * Возвращает кол-во страниц для категории
+	 * @return int
+	 */
+
+	public function getNumPages($cat_id){
+		$db = new PDO('mysql:host=colourshin.mysql;dbname=colourshin_db', 'colourshin_main', 'hrgmdjnd'); $db->exec('SET NAMES utf8');
+
+		$pRes = $db->query('SELECT COUNT(`id`) FROM `store` WHERE `cat_id` = "'.$cat_id.'"'); $pRes = $pRes->fetchAll();
+
+		$count = intval($pRes[0][0]);
+
+		$num_pages = ceil($count / $this->subjects_on_page);
+
+		return $num_pages;
 	}
 
 }
